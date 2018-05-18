4409770d0365fd370e0472e63fb038cbb7d9534c
Franck Nijhof <frenck@geekchimp.com>
2018-05-18 21:36:35 +0200

    :sparkles: Adds support for setting a password

--- jupyter/rootfs/etc/cont-init.d/50-password.sh
+++ jupyter/rootfs/etc/cont-init.d/50-password.sh
@@ -6,7 +6,12 @@
 # shellcheck disable=SC1091
 source /usr/lib/hassio-addons/base.sh
 
-# Basic idea on setting a password
-#password=$(python3 -c "from notebook.auth.security import passwd;print(passwd('test'))")
-#c.NotebookApp.password = u'%s'" % (pwhash)
+readonly CONFIG_PATH="/etc/jupyter/jupyter_notebook_config.py"
 
+# Set password
+if hass.config.has_value 'password'; then
+    password=$(hass.config.get 'password')
+    password=$(python3 -c "from notebook.auth.security import passwd;print(passwd('${password}'))")
+    sed -i "s/c.NotebookApp.password\ .*/c.NotebookApp.password\ =\ '${password}'/" "${CONFIG_PATH}" \
+        || hass.die 'Failed setting up password'
+fi
